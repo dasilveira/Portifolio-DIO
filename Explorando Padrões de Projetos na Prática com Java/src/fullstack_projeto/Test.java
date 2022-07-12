7d256cc0474c95519eb64aab088a83dc567fec77
Gabriel da Silveira <81595472+dasilveira@users.noreply.github.com>
2022-07-12 13:16:28 -0300

    Create Test.java

---
+++ Explorando Padrões de Projetos na Prática com Java/src/fullstack_projeto/Test.java
@@ -0,0 +1,57 @@
+package one.digitalinnovation.gof;
+
+import fullstack_projeto.Facade.Facade;
+import fullstack_projeto.Singleton.SingletonEager;
+import fullstack_projeto.Singleton.SingletonLazy;
+import fullstack_projeto.Singleton.SingletonLazyHolder;
+import fullstack_projeto.Strategy.Comportamento;
+import fullstack_projeto.Strategy.ComportamentoAgressivo;
+import fullstack_projeto.Strategy.ComportamentoDefensivo;
+import fullstack_projeto.Strategy.ComportamentoNormal;
+import fullstack_projeto.Strategy.Robo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		// Singleton
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		
+		// Strategy
+		
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(defensivo);
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		robo.mover();
+		robo.mover();
+		
+		// Facade
+		
+		Facade facade = new Facade();
+		facade.migrarCliente("Venilton", "14801788");
+	}
+
+}
